cfdcfaee8713e225ef19d1a6d1c71a5544c5b8e1
syed2024 <arsalanahmed186@hotmail.com>
2024-10-04 21:36:56 -0600

    Project 2
    Using HTML , CSS  , JavaScript

---
+++ Project2/script.js
@@ -0,0 +1,46 @@
+function colour(name) {
+    const body = document.getElementsByTagName("div")[0]; 
+    body.style.backgroundColor = name;
+   
+}
+
+
+const decrease = document.getElementById("decrease");
+const increase = document.getElementById("increase");
+const text = document.getElementById("text");
+
+let currentSize = 10;
+
+increase.addEventListener("click", () => {
+    if(currentSize < 100)
+    {
+        currentSize += 1;
+        text.style.fontSize = currentSize + "px";
+    }
+});
+
+
+decrease.addEventListener("click", () => {
+    
+    if(currentSize > 10)
+    {
+        currentSize -= 1;
+        text.style.fontSize = currentSize + "px";
+    }
+});
+
+
+const form = document.querySelector('form');
+form.addEventListener('submit' , function (e) {
+
+ e.preventDefault();
+ const A = parseInt(document.querySelector('#A').value);
+ const B = parseInt(document.querySelector('#B').value);
+
+ const result = document.querySelector('#results');
+
+ result.innerHTML = A+B;
+  prompt(A+B);
+
+
+});
